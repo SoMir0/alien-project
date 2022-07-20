9ee488f040c58c4f56f9a5baf2be2b021d25264f
Mirsen Kahrovic <mirsenkahrovic@gmail.com>
2022-07-20 16:30:29 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,110 @@
+<script>
+import Profile from './lib/Profile.svelte';
+
+let profileExtended = false;
+let chatExtended = true;
+</script>
+
+<main>
+  <aside class="profile" style={profileExtended ? "" : "left: -17rem"}>
+    <Profile />
+  </aside>
+  <div class="container" style={chatExtended ? "margin-right: 17rem" : ""}>
+    <button class="toggleButton left" on:click={() => {profileExtended = !profileExtended}}>{#if profileExtended}⤆{:else}⤇{/if}</button>
+    <button class="toggleButton right" on:click={() => {chatExtended = !chatExtended}}>{#if chatExtended}⤇{:else}⤆{/if}</button>
+    <div class="game-list">
+      <div class="game"></div>
+      <div class="game"></div>
+      <div class="game"></div>
+      <div class="game"></div>
+    </div>
+  </div>
+  <aside class="chat" style={chatExtended ? "" : "right: -17rem"}>
+    <h3>Chat</h3>
+    <input type="text" class="textinput" placeholder="Type your message..." />
+  </aside>
+</main>
+
+<style>
+main {
+  position: fixed; top: 0; left: 0;
+  width: 100vw; height: 100vh;
+}
+
+aside {
+  background: #eee;
+  transition: 200ms linear;
+  display: flex; flex-direction: column; align-items: center;
+  position: absolute;
+  width: 15rem;
+  height: 100%;
+  padding: 1rem;
+}
+
+aside:hover {
+  box-shadow: rgba(108, 108, 108, .5) 0 0 1rem;
+}
+
+.profile {
+  top: 0; left: 0;
+}
+
+.container {
+  display: flex; justify-content: center;
+  transition: margin 200ms linear;
+  padding: 1rem;
+}
+
+.container .game-list {
+  display: flex; flex-direction: row; flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.game {
+  background: #bbb;
+  width: 5rem; height: 5rem;
+  border-radius: 1rem;
+  transition: 150ms ease-in-out;
+}
+
+.game:hover {
+  transform: translatey(-10%);
+  box-shadow: #2229 0 5px 1rem;
+}
+
+.container .toggleButton {
+  position: absolute;
+  top: 0;
+  background: none;
+  outline: none; border: none;
+  border-radius: 0.5rem;
+  font-size: 1.5rem;
+  cursor: pointer;
+  z-index: 9;
+}
+
+.chat {
+  top: 0; right: 0;
+}
+
+.left {
+  left: 0;
+}
+
+.right {
+  right: 0;
+}
+
+.chat .textinput {
+  position: absolute; bottom: 1rem;
+  outline: none; border: none;
+  border-radius: 0.1rem;
+  transition: 100ms ease-in;
+  height: 1.5rem;
+  font-size: 1rem;
+}
+
+.chat .textinput:hover {
+  background: #ddd;
+}
+</style>
